4d1154703ad303d1a9717101f6af73db5db705d4
JakeEH <33324632+JakeEH@users.noreply.github.com>
2018-11-17 14:55:31 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTestTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var intetest = solution(91);
+        }
+        public static int solution(int n)
+        {
+            int[] d = new int[30];
+            int L = 0;
+            int p;
+            while (n > 0)
+            {
+                d[L] = n % 2;
+                n /= 2;
+                L++;
+                //L elements in array
+            }
+            // p is binary period
+            // d is binary array
+            for (p = 1; p <=L/2; p++)
+             //loop over elements in array (d)
+            {
+                int i;
+                bool ok = true;
+                for (i = 0; i < L - p; i++)
+                {
+                    if (d[i] != d[i + p])
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok)
+                {
+                    return p;
+                }
+            }
+            return -1;
+        }
+    }
+}
